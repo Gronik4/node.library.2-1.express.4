15d09def083f5e938abfc4830dd53b911a60cbef
Gronik4 <gronweb@yandex.ru>
2025-03-25 21:19:11 +0300

    Add routs

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ class Book{
   }
 }
 
-export const stor = {
+const stor = {
   books: [
     new Book(
       nanoid(7),
@@ -44,4 +44,26 @@ export const stor = {
     )
   ] 
 }
-console.log(stor.books);
+
+const app = express();
+app.use(express.json);
+
+app.post('/api/user/login', (res, req)=>{res.json({ id: 1, mail: "test@mail.ru" })});
+app.get('/api/books', (res, req)=>{
+  const {books} = stor;
+  console.log('id: 1, mail: "test@mail.ru" ');
+  res.json(books);
+});
+app.get('/api/books/:id', (res, req)=>{});
+app.post('/api/books', (res, req)=>{});
+app.put('/api/books/:id', (res, req)=>{});
+app.delete('/api/books/:id', (res, req)=>{});
+
+const port = process.env.PORT || 8080;
+app.listen(port, (err)=>{
+  if(err) {
+    return console.log(`string 65: ${err}`);
+  } else {console.log(`Server starting on port ${port}`);}
+});
+/*const {books} = stor;
+console.log(books);*/
